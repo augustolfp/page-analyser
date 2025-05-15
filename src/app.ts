74fcf1109bf06a77e775ee86efad6e3040fa66a3
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-05-15 19:14:48 -0300

    feat: add app router

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import express from "express";
 
 import cors from "cors";
 
+import router from "./routes/index.js";
 import errorHandlerMW from "./middlewares/errorHandlerMW.js";
 
 const app = express();
 app.use(express.json(), cors());
 
+app.use(router);
 app.use(errorHandlerMW);
 
 export default app;
