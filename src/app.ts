a30da2c19bf3df082478724d7eb8e4829fcf52aa
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-05-15 19:09:56 -0300

    feat: add middleware de erros

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,11 @@ import express from "express";
 
 import cors from "cors";
 
+import errorHandlerMW from "./middlewares/errorHandlerMW.js";
+
 const app = express();
 app.use(express.json(), cors());
+
+app.use(errorHandlerMW);
 
 export default app;
