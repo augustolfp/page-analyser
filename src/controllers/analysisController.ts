f6ba8e6ad744b3fdb9401e443d973aea31c79d30
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-05-15 21:26:27 -0300

    feat: instanciar puppeteer scraper para consulta de url

--- src/controllers/analysisController.ts
+++ src/controllers/analysisController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import puppeteer from "puppeteer";
 
 import decodeBase64Url from "../utils/decodeBase64Url.js";
 
@@ -6,6 +7,19 @@ export async function analyseUrl(req: Request, res: Response) {
     const base64Url: string = req.params.encodedBase64Url;
 
     const decodedUrl = decodeBase64Url(base64Url);
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(decodedUrl);
+    const pageElement = await page.$("#produtos-loop");
+
+    const produtosLoopContent = await pageElement?.evaluate((element) => {
+        return element.innerHTML;
+    });
+
+    console.log("loop content: ", produtosLoopContent);
+
+    await browser.close();
 
     const response = {
         url: decodedUrl,
