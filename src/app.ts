bec54b172b9128b419ead66c06138e466e594b8c
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-05-21 04:43:20 -0300

    feat: add handlebars

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "express-async-errors";
 import express from "express";
+import { engine } from "express-handlebars";
 
 import cors from "cors";
 
@@ -7,7 +8,23 @@ import router from "./routes/index.js";
 import errorHandlerMW from "./middlewares/errorHandlerMW.js";
 
 const app = express();
+
+// Servir arquivos estáticos (para importar css no handlebars):
+app.use(express.static("public"));
+
 app.use(express.json(), cors());
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+
+const teste = {
+    primeiraProp: "Olá mundo!",
+};
+
+app.get("/", (req, res) => {
+    res.render("home", { teste });
+});
 
 app.use(router);
 app.use(errorHandlerMW);
