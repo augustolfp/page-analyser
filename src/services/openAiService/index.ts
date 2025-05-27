c17aa2629a1e676a8cec722be0c26c4c3f53c202
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-05-27 01:33:40 -0300

    refactor: mover imageSlicer para arquivo separado

--- src/services/openAiService/index.ts
+++ src/services/openAiService/index.ts
@@ -4,41 +4,10 @@ import type {
     ResponseInputImage,
 } from "openai/src/resources/responses/responses.js";
 import { zodTextFormat } from "openai/helpers/zod.mjs";
-import sharp from "sharp";
 import { OutputFormat } from "./outputSchema.js";
+import imageSlicer from "./imageSlicer.js";
 
 import fs from "fs/promises";
-
-async function sliceScreenshot(pageScreenshotFilePath: string) {
-    const image = sharp(pageScreenshotFilePath);
-    const sliceHeight = 2048;
-    const { width: imageWidth, height: imageHeight } = await image.metadata();
-
-    const numberOfSlices = Math.floor(imageHeight / sliceHeight) + 1;
-    const slices = new Array(numberOfSlices).fill(0);
-    const imageRegions: sharp.Region[] = slices.map((_value, index) => {
-        const isLastSlice = numberOfSlices === index + 1;
-        const top = index * sliceHeight;
-        const height = isLastSlice ? imageHeight - top : sliceHeight;
-        const region = {
-            top: top,
-            height: height,
-            left: 0,
-            width: imageWidth,
-        };
-        return region;
-    });
-
-    const sliceImageArray = imageRegions.map(async (imageRegion) => {
-        const result = await sharp(pageScreenshotFilePath)
-            .extract(imageRegion)
-            .toBuffer();
-
-        return result.toString("base64");
-    });
-
-    return await Promise.all(sliceImageArray);
-}
 
 export async function getOpenAiPageAnalysis(pageScreenshotFilePath: string) {
     const prompt = await fs.readFile(
@@ -46,8 +15,9 @@ export async function getOpenAiPageAnalysis(pageScreenshotFilePath: string) {
         "utf-8",
     );
 
-    const slicedScreenshotImages = await sliceScreenshot(
+    const slicedScreenshotImages = await imageSlicer(
         pageScreenshotFilePath,
+        2048,
     );
 
     const imagesInputs: ResponseInputImage[] = slicedScreenshotImages.map(
