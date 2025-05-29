79e2e5e3a8ac437c3c7e93184c12408eeaf3db65
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-05-29 03:02:29 -0300

    refactor: padronizar tamanhos de fonte

--- src/services/pdfService/index.ts
+++ src/services/pdfService/index.ts
@@ -3,47 +3,72 @@ import fs from "fs";
 import { openAiResultExample } from "../../openAiResultExample.js";
 
 export default async function createPdf(fileName: string, content: string) {
+    const heading1 = {
+        size: 20,
+    };
+
+    const heading2 = {
+        size: 16,
+    };
+
+    const normalText = {
+        size: 10,
+    };
+
     const filePath = `generatedReports/${fileName}.pdf`;
-    const doc = new PDFDocument();
+
+    const doc = new PDFDocument({ size: "A4" });
+
     doc.pipe(fs.createWriteStream(filePath));
-    doc.fontSize(25).text(openAiResultExample.titulo, {
+
+    doc.fontSize(heading1.size).text(openAiResultExample.titulo, {
         align: "center",
     });
 
     doc.moveDown();
 
-    doc.fontSize(18).text("Introdução");
-    doc.fontSize(14).text(openAiResultExample.introducao);
+    doc.fontSize(heading2.size).text("Introdução");
+    doc.fontSize(normalText.size).text(openAiResultExample.introducao);
 
     doc.moveDown();
 
-    doc.fontSize(18).text("Avaliação das Categorias");
-    doc.fontSize(14).text(openAiResultExample.avaliacaoCategorias.textoInicial);
+    doc.fontSize(heading2.size).text("Avaliação das Categorias");
+    doc.fontSize(normalText.size).text(
+        openAiResultExample.avaliacaoCategorias.textoInicial,
+    );
 
     doc.moveDown();
 
-    doc.fontSize(18).text("Avaliação dos Produtos");
-    doc.fontSize(14).text(openAiResultExample.avaliacaoProdutos.textoInicial);
+    doc.fontSize(heading2.size).text("Avaliação dos Produtos");
+    doc.fontSize(normalText.size).text(
+        openAiResultExample.avaliacaoProdutos.textoInicial,
+    );
 
     doc.moveDown();
 
-    doc.fontSize(18).text("Pontos Positivos");
-    doc.fontSize(14).text(openAiResultExample.pontosPositivos.textoInicial);
+    doc.fontSize(heading2.size).text("Pontos Positivos");
+    doc.fontSize(normalText.size).text(
+        openAiResultExample.pontosPositivos.textoInicial,
+    );
 
     doc.moveDown();
 
-    doc.fontSize(18).text("Pontos Negativos");
-    doc.fontSize(14).text(openAiResultExample.pontosNegativos.textoInicial);
+    doc.fontSize(heading2.size).text("Pontos Negativos");
+    doc.fontSize(normalText.size).text(
+        openAiResultExample.pontosNegativos.textoInicial,
+    );
 
     doc.moveDown();
 
-    doc.fontSize(18).text("Sugestões de Melhorias");
-    doc.fontSize(14).text(openAiResultExample.sugestoesMelhorias.textoInicial);
+    doc.fontSize(heading2.size).text("Sugestões de Melhorias");
+    doc.fontSize(normalText.size).text(
+        openAiResultExample.sugestoesMelhorias.textoInicial,
+    );
 
     doc.moveDown();
 
-    doc.fontSize(18).text("Conclusão");
-    doc.fontSize(14).text(openAiResultExample.conclusao);
+    doc.fontSize(heading2.size).text("Conclusão");
+    doc.fontSize(normalText.size).text(openAiResultExample.conclusao);
 
     doc.end();
 
