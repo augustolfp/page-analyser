746c077bd52417c999ba73a805a6fc2d8e0a43bf
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-05-29 02:53:44 -0300

    feat: add textos-base no layout do PDF

--- src/services/pdfService/index.ts
+++ src/services/pdfService/index.ts
@@ -6,7 +6,45 @@ export default async function createPdf(fileName: string, content: string) {
     const filePath = `generatedReports/${fileName}.pdf`;
     const doc = new PDFDocument();
     doc.pipe(fs.createWriteStream(filePath));
-    doc.text(JSON.stringify(openAiResultExample));
+    doc.fontSize(25).text(openAiResultExample.titulo, {
+        align: "center",
+    });
+
+    doc.moveDown();
+
+    doc.fontSize(18).text("Introdução");
+    doc.fontSize(14).text(openAiResultExample.introducao);
+
+    doc.moveDown();
+
+    doc.fontSize(18).text("Avaliação das Categorias");
+    doc.fontSize(14).text(openAiResultExample.avaliacaoCategorias.textoInicial);
+
+    doc.moveDown();
+
+    doc.fontSize(18).text("Avaliação dos Produtos");
+    doc.fontSize(14).text(openAiResultExample.avaliacaoProdutos.textoInicial);
+
+    doc.moveDown();
+
+    doc.fontSize(18).text("Pontos Positivos");
+    doc.fontSize(14).text(openAiResultExample.pontosPositivos.textoInicial);
+
+    doc.moveDown();
+
+    doc.fontSize(18).text("Pontos Negativos");
+    doc.fontSize(14).text(openAiResultExample.pontosNegativos.textoInicial);
+
+    doc.moveDown();
+
+    doc.fontSize(18).text("Sugestões de Melhorias");
+    doc.fontSize(14).text(openAiResultExample.sugestoesMelhorias.textoInicial);
+
+    doc.moveDown();
+
+    doc.fontSize(18).text("Conclusão");
+    doc.fontSize(14).text(openAiResultExample.conclusao);
+
     doc.end();
 
     return filePath;
