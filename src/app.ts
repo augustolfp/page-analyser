4a790db92a9f63d8d498c4dd6ffc4eafcfb1e358
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-05-29 00:05:54 -0300

    feat: receber dados de email e nome do restaurante no payload da requisição

--- src/app.ts
+++ src/app.ts
@@ -8,22 +8,32 @@ const app = express();
 app.use(express.json(), cors());
 
 app.post("/analysis", async (req: Request, res: Response) => {
-    const url = req.body.url;
+    const { urlRestaurante, nomeRestaurante, emailCliente, nomeCliente } =
+        req.body;
 
     res.status(201).send(
         "Geração de relatório iniciada com sucesso. O resultado será recebido via e-mail.",
     );
 
     const gerandoReport = ora("Gerando report em PDF").start();
-    const { reportFilePath, imageFilePath } = await getPageReport(url);
+    const { reportFilePath, imageFilePath } =
+        await getPageReport(urlRestaurante);
     gerandoReport.succeed();
 
     const enviandoEmail = ora("Enviado report via email").start();
     await sendEmail(
-        "Augusto Lopes",
-        "augustolfp@gmail.com",
-        "Teste de SMTP",
-        "Boa tarde!",
+        nomeCliente,
+        emailCliente,
+        "Avaliação do Cardápio por IA",
+        `
+        Olá ${nomeCliente}!
+        \n
+        Segue em anexo o relatório do cardápio de ${nomeRestaurante}, gerado por Inteligência Artificial.
+        Esperamos que este traga insights valiosos.
+        \n
+        Atenciosamente,
+        Equipe Prefiro Delivery
+        `,
         [
             {
                 path: reportFilePath,
