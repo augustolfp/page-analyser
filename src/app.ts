c9a24013fc1e98f19e2df2863d449c2c6220cc48
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-05-28 23:44:18 -0300

    feat: implementar criação assíncrona de relatório via requisição POST

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { getPageReport } from "./services/pageReportService/index.js";
 import cors from "cors";
 import sendEmail from "./services/emailService/index.js";
+import ora from "ora";
 
 const app = express();
 app.use(express.json(), cors());
@@ -9,8 +10,15 @@ app.use(express.json(), cors());
 app.post("/analysis", async (req: Request, res: Response) => {
     const url = req.body.url;
 
-    const { reportFilePath, imageFilePath } = await getPageReport(url);
+    res.status(201).send(
+        "Geração de relatório iniciada com sucesso. O resultado será recebido via e-mail.",
+    );
 
+    const gerandoReport = ora("Gerando report em PDF").start();
+    const { reportFilePath, imageFilePath } = await getPageReport(url);
+    gerandoReport.succeed();
+
+    const enviandoEmail = ora("Enviado report via email").start();
     await sendEmail(
         "Augusto Lopes",
         "augustolfp@gmail.com",
@@ -25,8 +33,7 @@ app.post("/analysis", async (req: Request, res: Response) => {
             },
         ],
     );
-
-    res.status(201).send("Geração de relatório triggada");
+    enviandoEmail.succeed();
 });
 
 export default app;
